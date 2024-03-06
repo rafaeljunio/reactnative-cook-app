08e90acc96b382bbf236482077e10770950451d8
Rafael Souza <rafael.souza@iexfy.com>
2024-03-06 09:24:01 -0300

    setup

---
+++ README.md
@@ -0,0 +1,10 @@
+## Cook APP
+
+### Tecnologias utilizadas
+* React Native
+* StyleSheet
+* Supabase
+* Expo
+
+### Link Figma
+https://www.figma.com/file/nZxOFs2lgRRxNrurLvh3ea/Cook-App-(Community)-(Copy)?type=design&node-id=0%3A1&mode=dev&t=myQfyCpsL6DplW0K-1
---
+++ src/services/index.ts
@@ -0,0 +1,12 @@
+import * as recipes from "./recipesService"
+import * as ingredients from "./ingredientsService"
+import * as preparations from "./preparationsService"
+
+export const services = {
+  recipes,
+  ingredients,
+  preparations,
+  storage: {
+    imagePath: "https://xlwqjrmnloaeppcshkra.supabase.co/storage/v1/object/public/ingredients",
+  },
+}
